38f8bc53e25fd7d8b2e984dd895211310f4b5e8f
Marlan Perumal <marlan.perumal@gmail.com>
2025-01-22 17:16:46 +0200

    Completed tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,192 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { create } from "zustand";
+import { combine } from "zustand/middleware";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+const useGameStore = create(
+  combine(
+    {
+      history: [Array(9).fill(null)],
+      currentMove: 0,
+    },
+    (set) => ({
+      setHistory: (
+        nextHistory:
+          | Array<string | null>[]
+          | ((squares: Array<string | null>[]) => Array<string | null>[])
+      ) => {
+        set((state) => ({
+          history:
+            typeof nextHistory === "function"
+              ? nextHistory(state.history)
+              : nextHistory,
+        }));
+      },
+      setCurrentMove: (
+        nextCurrentMove: number | ((currentMove: number) => number)
+      ) => {
+        set((state) => ({
+          currentMove:
+            typeof nextCurrentMove === "function"
+              ? nextCurrentMove(state.currentMove)
+              : nextCurrentMove,
+        }));
+      },
+    })
   )
+);
+
+function calculateWinner(squares: Array<string | null>) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
-export default App
+function calculateTurns(squares: Array<string | null>) {
+  return squares.filter((square) => square !== null).length;
+}
+
+function calculateStatus(winner: string | null, turns: number, player: string) {
+  if (!winner && !turns) return "Draw";
+  if (winner) return `Winner: ${winner}`;
+  return `Next player: ${player}`;
+}
+
+function Square({
+  value,
+  onSquareClick,
+}: {
+  value: string | null;
+  onSquareClick: () => void;
+}) {
+  return (
+    <button
+      style={{
+        display: "inline-flex",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 0,
+        backgroundColor: "#fff",
+        border: "1px solid #999",
+        outline: 0,
+        borderRadius: 0,
+        fontSize: "1rem",
+        fontWeight: "bold",
+      }}
+      onClick={onSquareClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+function Board({
+  xIsNext,
+  squares,
+  onPlay,
+}: {
+  xIsNext: boolean;
+  squares: Array<string | null>;
+  onPlay: (nextSquares: Array<string | null>) => void;
+}) {
+  const winner = calculateWinner(squares);
+  const turns = calculateTurns(squares);
+  const player = xIsNext ? "X" : "O";
+  const status = calculateStatus(winner, turns, player);
+
+  function handleClick(i: number) {
+    if (squares[i] || winner) return;
+    const nextSquares = squares.slice();
+    nextSquares[i] = player;
+    onPlay(nextSquares);
+  }
+
+  return (
+    <div>
+      <div style={{ marginBottom: "0.5rem" }}>{status}</div>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3, 1fr)",
+          gridTemplateRows: "repeat(3, 1fr)",
+          width: "calc(3 * 2.5rem)",
+          height: "calc(3 * 2.5rem)",
+          border: "1px solid #999",
+        }}
+      >
+        {squares.map((square, squareIndex) => (
+          <Square
+            key={squareIndex}
+            value={square}
+            onSquareClick={() => handleClick(squareIndex)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function Game() {
+  const history = useGameStore((state) => state.history);
+  const setHistory = useGameStore((state) => state.setHistory);
+  const currentMove = useGameStore((state) => state.currentMove);
+  const setCurrentMove = useGameStore((state) => state.setCurrentMove);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares: Array<string | null>) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove: number) {
+    setCurrentMove(nextMove);
+  }
+
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "row", fontFamily: "monospace" }}
+    >
+      <div>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div style={{ marginLeft: "1rem" }}>
+        <ol>
+          {history.map((_, historyIndex) => {
+            const description =
+              historyIndex > 0
+                ? `Go to move #${historyIndex}`
+                : "Go to game start";
+            return (
+              <li key={historyIndex}>
+                <button onClick={() => jumpTo(historyIndex)}>
+                  {description}
+                </button>
+              </li>
+            );
+          })}
+        </ol>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return <Game />;
+}
+
+export default App;
